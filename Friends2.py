9d0d1c6339171db88f6e35af8054909f6644b88a
CeNeCheat <dedie.mike111@gmail.com>
2021-11-07 16:11:38 +0300

    DZ

--- Friends2.py
+++ Friends2.py
@@ -1,18 +1,27 @@
+list = []
+friends = [input("Добавьте трех друзей: ").capitalize(), input().capitalize(), input().capitalize()]
+for s in friends:
+    list.append(s)
 while True:
-    list = []
-    friends = [input("Добавьте трех друзей: ").capitalize(), input().capitalize(), input().capitalize()]
-    for s in friends:
-        list.append(s)
-    friend = input("Введите имя друга из списка")
-    try:
-        list.index(friend)
-    except ValueError:
-        print("Такого пользователя нет в списке")
+    user= input("Хотите добавить еще? ").capitalize()
+    if user == "Да" or user == "Yes":
+        friend = input("Введите имя друга: ").capitalize()
+        list.append(friend)
+        for s in list:
+            print(s)
+    elif user == "Нет" or user == "No":
+        print(list)
+        print(len(list))
         break
-    answer = input("Хотите, чтобы этот человек присутсвовал на вечеринке?").capitalize()
-    if answer == "Нет" or answer == 'No':
-        list.remove(friend)
-        print(list)
     else:
-        print(list)
-    break
+        print("Что ты ввел? Алло! Ответь да или нет! Не строй из себя умника!")
+user = input("Хотите удалить последнего в списке?: ").capitalize()
+if user == "Yes" or user == "Да":
+    list.remove(friend)
+    print(list)
+    print(len(list))
+elif user == "No" or user== "Нет":
+    print(list)
+    print(len(list))
+else:
+    print("Чево?")
